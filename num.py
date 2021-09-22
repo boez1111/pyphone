2e2e6ba577757384cb3638ac59a4b46d04fd7068
The Venom <76055736+boez1111@users.noreply.github.com>
2021-09-23 00:48:38 +0200

    num.py

--- num.py
+++ num.py
@@ -41,7 +41,7 @@ async def scan(ctx, number=None):
     print(answer)
     if answer["valid"] == True:
 
-        embed2 = discord.Embed(title=f"**Informationen über die Telefonnummer {phone_number}**!",descript>
+        embed2 = discord.Embed(title=f"**Information about the Number {phone_number}**",description="The information may not be 100% correct. Deviations possible.",
         embed2.add_field(name="Valid", value=answer['valid'], inline=False)
         embed2.add_field(name="Number", value=answer['number'], inline=False)
         embed2.add_field(name="Local Format", value=answer['local_format'], inline=False)
